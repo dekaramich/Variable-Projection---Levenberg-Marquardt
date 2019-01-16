9b7a62ee79a82ee1349cb9657700fcfa4dcaec6c
dekaramich <37862653+dekaramich@users.noreply.github.com>
2019-01-16 21:19:35 +0200

    Delete normalizationy_val.m

--- normalizationy_val.m
+++
@@ -1,8 +0,0 @@
-function [ynorm, maxvarytr, minvarytr] = normalizationy_val(y, a, b,minvarytr,maxvarytr);
-ytrT = y';
-
-Ly = (ytrT(1,:)- minvarytr)/(maxvarytr-minvarytr);
-ytrn(1,:) = a+Ly*(b-a);
-
-ynorm = ytrn';
-end
